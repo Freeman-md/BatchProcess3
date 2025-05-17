4c15ba58766d202b72330e9a6d590c0631fd3243
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-05-17 07:08:19 +0100

    bind the svg image to a property in main view model and resize side bar based on sidebarexpanded property

--- MainView.axaml.cs
+++ MainView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using BatchProcess3.ViewModels;
 
 namespace BatchProcess3;
 
@@ -7,5 +9,13 @@ public partial class MainView : Window
     public MainView()
     {
         InitializeComponent();
+    }
+
+    private void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.ClickCount != 2)
+            return;
+        
+        (DataContext as MainViewModel)?.SideMenuResizedCommand.Execute(null);
     }
 }
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,9 +1,25 @@
+using Avalonia.Svg.Skia;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace BatchProcess3.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
-    private string _test = "test";
+    [NotifyPropertyChangedFor(nameof(SideMenuImage))]
+    private bool _sideMenuExpanded = true;
+
+    
+    public SvgImage SideMenuImage => new SvgImage
+    {
+        Source = SvgSource.Load($"avares://{nameof(BatchProcess3)}/Assets/Images/{(SideMenuExpanded ? "logo" : "icon")}.svg")
+    };
+
+    [RelayCommand]
+    private void SideMenuResized()
+    {
+        SideMenuExpanded = !SideMenuExpanded;
+    }
+    
 }
