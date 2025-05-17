98767a8facb81502cbf9d0061d0d262e7f901604
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2025-05-17 07:12:17 +0100

    display app logos based on side menu expaneded property

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -7,14 +7,7 @@ namespace BatchProcess3.ViewModels;
 public partial class MainViewModel : ViewModelBase
 {
     [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(SideMenuImage))]
     private bool _sideMenuExpanded = true;
-
-    
-    public SvgImage SideMenuImage => new SvgImage
-    {
-        Source = SvgSource.Load($"avares://{nameof(BatchProcess3)}/Assets/Images/{(SideMenuExpanded ? "logo" : "icon")}.svg")
-    };
 
     [RelayCommand]
     private void SideMenuResized()
